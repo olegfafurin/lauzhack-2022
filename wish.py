24c4a193f1fa8c260b6907cbd030a6c0e97b2dc8
Oleg Fafurin <oleg.fafurin@epfl.ch>
2022-12-03 19:37:37 +0100

    add wish

---
+++ wish.py
@@ -0,0 +1,18 @@
+class Wish:
+    def __init__(self):
+        self.name = None
+        self.price = None
+        self.photo = None
+        self.desc = None
+
+    def update_name(self, name):
+        self.desc = name
+
+    def update_photo(self, photo):
+        self.photo = photo
+
+    def update_price(self, price):
+        self.price = price
+
+    def update_desc(self, desc):
+        self.desc = desc
