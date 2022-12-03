a93d9cd97677df08d75b58433429f2c957fde1c6
Anton Maksimov <antmaxi@ya.ru>
2022-12-03 16:05:04 +0100

    Add connections tables (without foreign key yet)

--- db.py
+++ db.py
@@ -1,9 +1,18 @@
+import logging
 import sqlite3
+from abc import ABC, abstractmethod
+from contextlib import contextmanager
+from enum import Enum
 from typing import Optional
 
-from contextlib import contextmanager
-
 DB_PATH = "wishlist.db"
+
+logging.basicConfig(level=logging.DEBUG, format='%(message)s')  # TODO make set up in main.py
+logger = logging.getLogger(__name__)
+
+
+class RelationType(Enum):
+    FRIEND = "friend"
 
 
 @contextmanager
@@ -15,16 +24,36 @@ def db_ops(db_name):
     conn.close()
 
 
-class Creator:
+class Table(ABC):
     def __init__(self, db_path: str = DB_PATH):
         self.db_path = db_path
+        self.table_name = ""
+
+    def delete(self):
+        with db_ops(self.db_path) as cur:
+            cur.execute(f"DROP TABLE IF EXISTS {self.table_name}")
+
+    @abstractmethod
+    def create_table(self):
+        pass
+
+    @abstractmethod
+    def add(self, *args, **kwargs):
+        pass
+
+
+class Creator(Table):
+    def __init__(self):
+        super().__init__()
         self.table_name = "creator"
 
     def create_table(self) -> None:
         with db_ops(self.db_path) as cur:
-            query = f"CREATE TABLE IF NOT EXISTS {self.table_name}(" \
-                        f"telegram_id INT" \
-                        f")"
+            query = f"""CREATE TABLE IF NOT EXISTS {self.table_name} 
+                    ( 
+                        telegram_id INT 
+                        deadline DATE 
+                    )"""
             cur.execute(query)
 
     def add(self,
@@ -39,16 +68,18 @@ class Creator:
                 """, [telegram_id, deadline])
 
 
-class Presenter:
-    def __init__(self, db_path: str = DB_PATH):
-        self.db_path = db_path
+class Presenter(Table):
+    def __init__(self):
+        super().__init__()
         self.table_name = "presenter"
 
     def create_table(self) -> None:
         with db_ops(self.db_path) as cur:
-            query = f"CREATE TABLE IF NOT EXISTS {self.table_name}(" \
-                        f"telegram_id INT," \
-                        f"name TEXT)"
+            query = f"""CREATE TABLE IF NOT EXISTS {self.table_name} 
+                    ( 
+                        telegram_id INT, 
+                        name TEXT 
+                    )"""
             cur.execute(query)
 
     def add(self, telegram_id: int) -> None:
@@ -60,27 +91,23 @@ class Presenter:
                 """, [telegram_id, ])
 
 
-class Wish:
-    def __init__(self, db_path: str = DB_PATH):
-        self.db_path = db_path
+class Wish(Table):
+    def __init__(self):
+        super().__init__()
         self.table_name = "wish"
-
-    def delete(self):
-        with db_ops(self.db_path) as cur:
-            cur.execute(f"DROP TABLE IF EXISTS {self.table_name}")
 
     def create_table(self) -> None:
         with db_ops(self.db_path) as cur:
-            query = f"CREATE TABLE IF NOT EXISTS {self.table_name}" \
-                    f"(" \
-                    f"name TEXT," \
-                    f"booked BOOLEAN," \
-                    f"presented BOOLEAN," \
-                    f"priority INT," \
-                    f"link TEXT," \
-                    f"price REAL," \
-                    f"quantity INT" \
-            f")"
+            query = f"""CREATE TABLE IF NOT EXISTS {self.table_name}
+                    (
+                        name TEXT,
+                        booked BOOLEAN,
+                        presented BOOLEAN,
+                        priority INT,
+                        link TEXT,
+                        price REAL,
+                        quantity INT
+                    )"""
             cur.execute(query)
 
     # TODO: add photo
@@ -99,14 +126,74 @@ class Wish:
                 """, [name, priority, link, price, quantity])
 
 
-if __name__ == "__main__":
-    # tests?
+class Friendship(Table):
+    def __init__(self):
+        super().__init__()
+        self.table_name = "friendship"
+
+    def create_table(self) -> None:
+        with db_ops(self.db_path) as cur:
+            query = f"""CREATE TABLE IF NOT EXISTS {self.table_name} 
+                    ( 
+                    creator_id INT, 
+                    presenter_id INT, 
+                    type TEXT 
+                    )"""
+            cur.execute(query)
+
+    def add(self,
+            creator_id: int,
+            presenter_id: int,
+            relation_type: RelationType = RelationType.FRIEND,
+            ) -> None:
+        with db_ops(self.db_path) as cur:
+            cur.execute(
+                f"""
+                INSERT INTO {self.table_name} VALUES
+                    (?, ?, ?)
+                """, [creator_id, presenter_id, relation_type.value])
+
+
+class Present(Table):
+    def __init__(self):
+        super().__init__()
+        self.table_name = "present"
+
+    def create_table(self) -> None:
+        with db_ops(self.db_path) as cur:
+            query = f"""CREATE TABLE IF NOT EXISTS {self.table_name} 
+                    ( 
+                        creator_id INT, 
+                        presenter_id INT, 
+                        present_id INT
+                    )"""
+            cur.execute(query)
+
+    def add(self,
+            creator_id: int,
+            presenter_id: int,
+            present_id: int,
+            ) -> None:
+        with db_ops(self.db_path) as cur:
+            cur.execute(
+                f"""
+                INSERT INTO {self.table_name} VALUES
+                    (?, ?, ?)
+                """, [creator_id, presenter_id, present_id])
+
+
+# TODO make test normal without using actual db
+def test_wish() -> None:
     wish = Wish()
     wish.delete()
     wish.create_table()
     wish.add(name="bla")
     wish.add(name="blablab", quantity=5)
     with db_ops(DB_PATH) as cur:
-        for row in cur.execute(f"SELECT name, quantity FROM {wish.table_name}"):
-            print(row)
+        rows = list(cur.execute(f"SELECT name, quantity FROM {wish.table_name}"))
+        assert rows[0] == ("bla", None)
+        assert rows[1] == ("blablab", 5)
 
+
+if __name__ == "__main__":
+    test_wish()
